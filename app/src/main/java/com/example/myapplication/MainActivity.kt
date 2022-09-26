013a8657a682d92b00266c8c360702b176d3ae38
Junghwa Lee <lsll3949@naver.com>
2022-09-26 13:55:52 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var button1: Button
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button1 = findViewById<Button>(R.id.button1)
+
+        button1.setOnClickListener {
+            Toast.makeText(applicationContext, "버튼을 눌렀어요",
+                    Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
